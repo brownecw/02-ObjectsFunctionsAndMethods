51922d5550554764154b12c8fc67f004e6f1781d
brownecw <brownecw@rose-hulman.edu>
2018-11-28 09:50:01 -0500

    part 2.2 done

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Colin Browne.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -21,14 +21,16 @@ import rosegraphics as rg
 
 
 def main():
+    print(triangle(3, 4))
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
     """
-
+    # terribleturtle('green' , 4)
+    terribleturtle('red' , 7)
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# DONE: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -36,14 +38,18 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE: 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
+import math
+def triangle(a , b):
+    return(math.sqrt(a**2 + b**2))
+
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -65,10 +71,21 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
+# NEW: call the function once, comment it out, then call it again with different values
 ###############################################################################
+import rosegraphics as rg
+
+def terribleturtle(color , girth):
+    window = rg.TurtleWindow()
+    sam = rg.SimpleTurtle('turtle')
+    sam.pen = rg.Pen(color, girth)
+    sam.speed = 4
+    sam.forward(100)
+    window.close_on_mouse_click()
+
 
 
 ###############################################################################
